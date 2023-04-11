8872303841582368f94b68b39e09b321245b4da2
truong cong bao <bao077559216@gmail.com>
2023-04-11 10:31:58 +0700

    lam from dang nhap

--- Web mua bán sách/Web mua bán sách/Dang_nhap.Designer.cs
+++ Web mua bán sách/Web mua bán sách/Dang_nhap.Designer.cs
@@ -20,28 +20,28 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+            #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // Dang_nhap
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Dang_nhap";
-            this.Text = "Dang_nhap";
-            this.Load += new System.EventHandler(this.Dang_nhap_Load);
-            this.ResumeLayout(false);
+            /// <summary>
+            /// Required method for Designer support - do not modify
+            /// the contents of this method with the code editor.
+            /// </summary>
+            private void InitializeComponent()
+            {
+                this.SuspendLayout();
+                // 
+                // Dang_nhap
+                // 
+                this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.ClientSize = new System.Drawing.Size(800, 450);
+                this.Name = "Dang_nhap";
+                this.Text = "Dang_nhap";
+                this.Load += new System.EventHandler(this.Dang_nhap_Load);
+                this.ResumeLayout(false);
 
-        }
+            }
 
-        #endregion
+            #endregion
     }
 }
