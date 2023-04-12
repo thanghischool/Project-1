f242f3eaf5ebbf660f8122ebfa44f6467facc69c
Nguyễn Thiện Thắng <thangxpro999@gmail.com>
2023-04-12 12:30:48 +0700

    Thêm form Quản Lý

---
+++ Web mua bán sách/Web mua bán sách/Quan_Ly_Thong_Tin_San_Pham.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Web_mua_bán_sách
+{
+    public partial class Quan_Ly_Thong_Tin_San_Pham : Form
+    {
+        MySqlConnection conn;
+        MySqlCommandBuilder cmd;
+        MySqlDataAdapter adapter;
+        DataTable mytable;
+        string strconn = "Server = localhost; Database = Quan_Ly_Thong_Tin; UId = root; Pwd = 123456; Pooling-false;Character Set=utf8";
+        public Quan_Ly_Thong_Tin_San_Pham()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Quan_Ly_Thong_Tin_San_Pham_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySqlConnection("");
+            }
+            catch 
+            {
+                MessageBox.Show("Lỗi kết Nối", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
--- Web mua bán sách/Web mua bán sách/Trang_chu.cs
+++ Web mua bán sách/Web mua bán sách/Trang_chu.cs
@@ -62,7 +62,7 @@ namespace Web_mua_bán_sách
 
         private void thoátChươngTrìnhToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát chuwong trình", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát chương trình", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if(result == DialogResult.Yes)
             {
                 Close();
